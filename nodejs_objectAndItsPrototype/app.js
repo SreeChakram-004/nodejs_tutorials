196766b6c784122b3686158db4da5b04503b2cc0
SreeChakram-004 <cherrytheraider@gmail.com>
2022-06-24 11:41:24 +0530

    updated

---
+++ nodejs_objectAndItsPrototype/app.js
@@ -0,0 +1,19 @@
+const person = {
+    firstname: 'Siri',
+    lastname: 'Doe', 
+    greet : function (){
+       return 'Hey'+' '+this.firstname+' '+this.lastname
+    }
+}
+
+const sonu = Object.create(person);
+      sonu.firstname='Sonu';
+      sonu.lastname='Doe';
+
+const rita = Object.create(person);
+      rita.firstname='Rita';
+      rita.lastname='Doe';
+
+console.log(person.greet()); //Hey siri Doe
+console.log(sonu.greet()); //Hey Sonu Doe
+console.log(rita.greet()); //Hey Rita Doe
