5484d961bad3d7c0169f946c29b64cd7acb86b59
SreeChakram-004 <cherrytheraider@gmail.com>
2022-06-24 11:09:04 +0530

    updated

--- nodejs_EventEmitter/app.js
+++ nodejs_EventEmitter/app.js
@@ -1,15 +1,18 @@
 const Emitter = require('events');
+const eventConfig = require('./greet');
 
 const emtr = new Emitter();
 
-emtr.on('greet',function (){
+emtr.on(eventConfig.events.GREET,function (){
     console.log('somewhere, someone said hello');//second output
 });
 
-emtr.on('greet',function(){
+emtr.on(eventConfig.events.GREET,function(){
     console.log('a greeting occured!');//third output
 });
 
 console.log('Hello');//first output
 
-emtr.emit('greet');
+emtr.emit(eventConfig.events.GREET);
+console.log(eventConfig.events); //{ GREET: 'greet' }
+console.log(eventConfig.events.GREET); //greet
