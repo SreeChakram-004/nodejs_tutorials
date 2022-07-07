2185995edd9ea8bb00dfe783d6744d4756b8cc55
SreeChakram-004 <cherrytheraider@gmail.com>
2022-07-07 18:20:28 +0530

    updated

---
+++ nodejs_eventEmitter2/app.js
@@ -0,0 +1,14 @@
+const Emitter = require('./emitter');
+
+const eventEmitter = new Emitter;
+
+eventEmitter.on('greet', () => {
+    console.log('Hello!');
+});
+
+eventEmitter.on('greet', () => {
+    console.log('Hi!');
+});
+
+console.log('Greet happend!');
+eventEmitter.emit('greet');
